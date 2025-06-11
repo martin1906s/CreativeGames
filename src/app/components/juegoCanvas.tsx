a6ba873d3d695eb58dfe4733c2825fb0488ab883
martin1906s <martin.simbana007@gamil.com>
2025-06-11 08:41:36 -0500

    Css title

--- src/app/components/juegoCanvas.tsx
+++ src/app/components/juegoCanvas.tsx
@@ -1,6 +1,6 @@
 export default function JuegoCanvas() {
     return (
-        <div style={{ width: '100%', height: '100vh' }}>
+        <div className="juego-piano">
             <iframe 
                 src="/Piano/index.html" 
                 width="100%" 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,12 @@ export default function Home() {
   return (
     <main>
       <header>
-        <h1>Creative Games</h1>
-        <p>La mejor forma de aprender siempre es divirtiéndose, y qué mejor forma de hacerlo que a través del juego. Ahora tenemos la suerte de disponer de muchos juegos online para niños y, además, ¡gratis! Tienes una variedad de juegos adaptados para niños de 3 a 5 años de edad. ¡Seguro que a tus niños les encantarán!</p>
-        <h2>Juegos online para niños </h2> 
+        <div className="title">
+          <h1>Creative Games</h1>
+        </div>
+        <p>La mejor forma de aprender siempre es divirtiéndose, y qué mejor forma de hacerlo que a través del juego. Ahora tenemos la suerte de disponer de muchos juegos online para niños y, además, ¡Gratis!. </p>
+        <p>Tienes una variedad de juegos adaptados para niños de 3 a 5 años de edad. ¡Seguro que a tus niños les encantarán!</p>
+        <h2>Juegos online para niños </h2>
         <p>Los juegos para niños de 3 a 5 años son actividades pensadas para unir la diversión con el aprendizaje. A esta edad, los pequeños exploran el mundo mientras fortalecen habilidades como la memoria, la atención y el razonamiento lógico. También comienzan a identificar colores, formas y números, sentando las bases para aprendizajes futuros. Aquí encontrarás una amplia selección de juegos educativos gratuitos para niños de 3 a 5 años, creados para apoyar su crecimiento y despertar su curiosidad.</p>
         <h2>¿Para que sirven?</h2>
         <p> Estos ayudarán a los niños a mejorar su atención y concentración, así como su habilidad estratégica y su memoria visual. También trabajarán su afán de superación al querer mejorar sus resultados y estimularán su creatividad. </p>
